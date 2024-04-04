763f3a162ac664c3464162eeed1b130e7654ee52
vinh15cm <147390597+vinh15cm@users.noreply.github.com>
2024-04-04 07:57:03 +0700

    Add files via upload

---
+++ bài 1.js
@@ -0,0 +1,12 @@
+console.log("wellcome to Rikkei Academy");
+document.write('Quyết tâm học lập trình javascript');
+
+prompt("mời bạn nhập số tuổi của mình");
+alert('Chào mừng bạn đến với học viện Rikkei Academy');
+var result = confirm('Bạn đã đủ 18 tuổi chưa!');
+
+if (result) {
+    alert('Chúc mừng! Bạn đã đủ 18 tuổi.');
+} else {
+    alert('Xin lỗi! Bạn chưa đủ 18 tuổi.');
+}
---
+++ bài 2.js
@@ -0,0 +1,39 @@
+var firstName = 'Academy';
+var lastName = 'Rikkei'
+var fullName = lastName + ' ' + firstName;
+/* các kiểu dữ liệu trong JS
+1, dữ liệu kiểu chuỗi (string)
+-dùng để lưu trữ văn bản
+-chuỗi được bao quanh bởi dấu nháy đơn hoặc nháy kép
+ví dụ :
+var name ="vinh15cm"
+2,dữ liệu kiểu Number(số)
+-lưu trữ số
+ví dụ :
+var age = 19;
+3,dữ liệu kiểu boolean(logic)
+là kiểu dữ liệu chỉ có hai giá trị true/false thường dùng để biểu duễn các điều kiện logic
+var isStudent = true
+4,kiểu dữ liệu mảng (array)
+-là kiểu dữ liệu lưu trữ danh sách các phần tử cùng hoặc khác kiểu dữ liệu
+var numbers = [1,2,3,4,5];
+5,dữ liệu kiểu Object(đối tượng)
+-là kiểu dữ liệu dùng đê lưu trữ thông tin dưới dạng cặp key-value(khoá-dữ liệu)
+var person ={
+    firstName: 'Jane',
+    lastName: 'doe',
+    age :30
+}
+6,dữ liệu Function(hàm)
+là kiểu dữ liệu để định nghĩa hàm hoặc phương thức
+function greet(name){
+    return 'hello , ' + name + '!';
+}
+7,kiểu dữ liệu undefined
+- đại diễn cho dữ liệu không đc gán hoặc không tồn tại
+var x;
+console.log(x);
+8,kiểu dữ liệu Null
+-đại diện cho giá trị không tồn tại hoặc không có giá trị
+var y = null;
+*/
